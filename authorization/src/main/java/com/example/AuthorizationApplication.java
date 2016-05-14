3ae210428aed8982540b3ebb5e9816a7aaba9d56
making <makingx@gmail.com>
2016-05-15 00:04:45 +0900

    Support JWT

--- authorization/src/main/java/com/example/AuthorizationApplication.java
+++ authorization/src/main/java/com/example/AuthorizationApplication.java
@@ -1,21 +1,32 @@
 package com.example;
 
 import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
-@EnableAuthorizationServer
 @RestController
 public class AuthorizationApplication {
 
@@ -32,6 +43,44 @@ public class AuthorizationApplication {
             registry.addViewController("/").setViewName("index");
         }
     }
+
+    @Configuration
+    @EnableAuthorizationServer
+    @EnableConfigurationProperties({AuthorizationServerProperties.class})
+    static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        AuthenticationManager authenticationManager;
+        @Autowired
+        AuthorizationServerProperties authorizationServerProperties;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("demo")
+                    .secret("demo")
+                    .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                    .scopes("read", "write")
+                    .accessTokenValiditySeconds((int) TimeUnit.HOURS.toSeconds(1))
+                    .autoApprove(true);
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            security.tokenKeyAccess(authorizationServerProperties.getTokenKeyAccess());
+        }
+
+        @Bean
+        @ConfigurationProperties("jwt")
+        JwtAccessTokenConverter jwtAccessTokenConverter() {
+            return new JwtAccessTokenConverter();
+        }
+    }
+
 
     @Configuration
     @Order(-20)
