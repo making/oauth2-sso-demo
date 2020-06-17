8d253c7c978625714e46054c0c5d8163bb52e049
Toshiaki Maki <tmaki@pivotal.io>
2020-06-17 16:04:06 +0900

    Refactor

--- todo-api/src/main/java/lol/maki/dev/todo/Todo.java
+++ todo-api/src/main/java/lol/maki/dev/todo/Todo.java
@@ -1,74 +1,103 @@
 package lol.maki.dev.todo;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Todo {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String todoId;
-    private String todoTitle;
-    private boolean finished = false;
-    private Instant createdAt;
-    private String createdBy;
-    private Instant updatedAt;
-    private String updatedBy;
+	private String todoId;
 
-    public String getTodoId() {
-        return todoId;
-    }
+	private String todoTitle;
 
-    public void setTodoId(String todoId) {
-        this.todoId = todoId;
-    }
+	private boolean finished = false;
 
-    public String getTodoTitle() {
-        return todoTitle;
-    }
+	private Instant createdAt;
 
-    public void setTodoTitle(String todoTitle) {
-        this.todoTitle = todoTitle;
-    }
+	private String createdBy;
 
-    public boolean isFinished() {
-        return finished;
-    }
+	private Instant updatedAt;
 
-    public void setFinished(boolean finished) {
-        this.finished = finished;
-    }
+	private String updatedBy;
 
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
+	public Todo initializedBy(String createdBy) {
+		this.setTodoId(UUID.randomUUID().toString());
+		this.setCreatedAt(Instant.now());
+		this.setCreatedBy(createdBy);
+		this.setUpdatedAt(this.getCreatedAt());
+		this.setUpdatedBy(this.getCreatedBy());
+		return this;
+	}
 
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
+	public Todo updatedBy(String todoTitle, boolean finished, String updatedBy) {
+		if (todoTitle != null) {
+			this.setTodoTitle(todoTitle);
+		}
+		if (!Objects.equals(finished, this.isFinished())) {
+			this.setFinished(finished);
+		}
+		this.setUpdatedAt(Instant.now());
+		this.setUpdatedBy(updatedBy);
+		return this;
+	}
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
+	public String getTodoId() {
+		return todoId;
+	}
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
+	public void setTodoId(String todoId) {
+		this.todoId = todoId;
+	}
 
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
+	public String getTodoTitle() {
+		return todoTitle;
+	}
 
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+	public void setTodoTitle(String todoTitle) {
+		this.todoTitle = todoTitle;
+	}
 
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
+	public boolean isFinished() {
+		return finished;
+	}
 
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
 }
--- todo-api/src/main/java/lol/maki/dev/todo/TodoController.java
+++ todo-api/src/main/java/lol/maki/dev/todo/TodoController.java
@@ -1,72 +1,64 @@
 package lol.maki.dev.todo;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.time.Instant;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("todos")
 @CrossOrigin
 public class TodoController {
-    private final TodoRepository todoRepository;
+	private final TodoRepository todoRepository;
 
-    public TodoController(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
+	public TodoController(TodoRepository todoRepository) {
+		this.todoRepository = todoRepository;
+	}
 
-    @GetMapping(path = "")
-    public ResponseEntity<List<Todo>> getTodos() {
-        final List<Todo> todos = this.todoRepository.findAll();
-        return ResponseEntity.ok(todos);
-    }
+	@GetMapping(path = "")
+	public ResponseEntity<List<Todo>> getTodos() {
+		final List<Todo> todos = this.todoRepository.findAll();
+		return ResponseEntity.ok(todos);
+	}
 
-    @GetMapping(path = "/{todoId}")
-    public ResponseEntity<Todo> getTodo(@PathVariable("todoId") String todoId) {
-        final Optional<Todo> todo = this.todoRepository.findById(todoId);
-        return ResponseEntity.of(todo);
-    }
+	@GetMapping(path = "/{todoId}")
+	public ResponseEntity<Todo> getTodo(@PathVariable("todoId") String todoId) {
+		final Optional<Todo> todo = this.todoRepository.findById(todoId);
+		return ResponseEntity.of(todo);
+	}
 
-    @PostMapping(path = "")
-    public ResponseEntity<Todo> postTodos(@RequestBody Todo todo, @AuthenticationPrincipal Jwt jwt, UriComponentsBuilder builder) {
-        todo.setTodoId(UUID.randomUUID().toString());
-        todo.setCreatedAt(Instant.now());
-        todo.setCreatedBy(jwt.getSubject());
-        todo.setUpdatedAt(todo.getCreatedAt());
-        todo.setUpdatedBy(todo.getCreatedBy());
-        final Todo created = this.todoRepository.create(todo);
-        final URI uri = builder.pathSegment("todos", created.getTodoId()).build().toUri();
-        return ResponseEntity.created(uri).body(created);
-    }
+	@PostMapping(path = "")
+	public ResponseEntity<Todo> postTodos(@RequestBody Todo todo, @AuthenticationPrincipal Jwt jwt, UriComponentsBuilder builder) {
+		final Todo initialized = todo.initializedBy(jwt.getSubject());
+		final Todo created = this.todoRepository.create(initialized);
+		final URI uri = builder.pathSegment("todos", created.getTodoId()).build().toUri();
+		return ResponseEntity.created(uri).body(created);
+	}
 
-    @PutMapping(path = "/{todoId}")
-    public ResponseEntity<Todo> putTodo(@PathVariable("todoId") String todoId, @RequestBody Todo todo, @AuthenticationPrincipal Jwt jwt) {
-        final Optional<Todo> updated = this.todoRepository.findById(todoId)
-                .map(t -> {
-                    if (todo.getTodoTitle() != null) {
-                        t.setTodoTitle(todo.getTodoTitle());
-                    }
-                    if (!Objects.equals(todo.isFinished(), t.isFinished())) {
-                        t.setFinished(todo.isFinished());
-                    }
-                    t.setUpdatedAt(Instant.now());
-                    t.setUpdatedBy(jwt.getSubject());
-                    return this.todoRepository.updateById(t);
-                });
-        return ResponseEntity.of(updated);
-    }
+	@PutMapping(path = "/{todoId}")
+	public ResponseEntity<Todo> putTodo(@PathVariable("todoId") String todoId, @RequestBody Todo todo, @AuthenticationPrincipal Jwt jwt) {
+		final Optional<Todo> updated = this.todoRepository.findById(todoId)
+				.map(t -> t.updatedBy(todo.getTodoTitle(), todo.isFinished(), jwt.getSubject()))
+				.map(this.todoRepository::updateById);
+		return ResponseEntity.of(updated);
+	}
 
-    @DeleteMapping(path = "/{todoId}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable("todoId") String todoId) {
-        this.todoRepository.deleteById(todoId);
-        return ResponseEntity.noContent().build();
-    }
+	@DeleteMapping(path = "/{todoId}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable("todoId") String todoId) {
+		this.todoRepository.deleteById(todoId);
+		return ResponseEntity.noContent().build();
+	}
 }
