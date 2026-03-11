d8bf5964c8ff9251586ceced205290033b592dd2
Toshiaki Maki <makingx@gmail.com>
2026-03-11 17:42:52 +0900

    feat: add Micrometer configuration to filter specific metrics

---
+++ authorization/src/main/java/lol/maki/dev/authorization/config/MicrometerConfig.java
@@ -0,0 +1,22 @@
+package lol.maki.dev.authorization.config;
+
+import io.micrometer.core.instrument.config.MeterFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class MicrometerConfig {
+
+	@Bean
+	public MeterFilter meterFilter() {
+		return MeterFilter.deny(id -> {
+			String uri = id.getTag("uri");
+			if (uri == null) {
+				uri = id.getTag("url.path");
+			}
+			return uri != null && (uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator")
+					|| uri.startsWith("/_static"));
+		});
+	}
+
+}
